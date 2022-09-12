8a4d9a0c7fca315297e5dbe9924fdc68de1252b2
Mendy <mendyw94@gmail.com>
2022-09-12 09:00:10 -0400

    commit tree d2519af4e2624f46a8ce1dd3dab28950a5e9838b

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function Button () {
+    function blur() {
+        console.log('Good!')
+    }
+    return (
+        <button onFocus={blur} onBlur={() => console.log("Hey! Eyes on me!")}>Eyes on me</button>
+    )
+}
+
+export default Button
